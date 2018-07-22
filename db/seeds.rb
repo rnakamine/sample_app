a377f37ed2665d5b29e5f69ba0fcaf7afba1385b
Ryo Nakamine <rnakamine8080@gmail.com>
2018-07-22 22:56:22 +0900

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ユーザー
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
-             password: "foobar",
-             password_confirmation: "foobar",
+             password: "password",
+             password_confirmation: "password",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
@@ -25,8 +27,17 @@ User.create!(name: "Example User",
               activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each {|user| user.microposts.create!(content: content)}
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
